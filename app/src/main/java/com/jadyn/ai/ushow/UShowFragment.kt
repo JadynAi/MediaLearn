e4ebd5be721f9cd865cc2d2fa7fefbffaddc44b5
JadynAI <936745006@qq.com>
2019-09-12 22:55:03 +0800

    代码新增UShowFragment

---
+++ app/src/main/java/com/jadyn/ai/ushow/UShowFragment.kt
@@ -0,0 +1,18 @@
+package com.jadyn.ai.ushow
+
+import com.jadyn.ai.kotlind.base.BaseFragment
+import com.jadyn.ai.medialearn.R
+
+/**
+ *@version:
+ *@FileDescription:
+ *@Author:Jing
+ *@Since:2019-09-12
+ *@ChangeList:
+ */
+class UShowFragment : BaseFragment() {
+    override fun getResId(): Int {
+        return R.layout.fragment_ushow
+    }
+
+}
